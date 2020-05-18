86a44245e967cfae792e3b4e9939de8e90ca549f
Adam Kurczynski <adkurczynskis@interia.pl>
2020-05-18 22:29:18 +0200

    Updates

---
+++ precache-manifest.c0a7615d2057352465752fa0e9919188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea87d6485a0420a3d685cb2fd68955e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "db60844c964a08055936",
+    "url": "/monsters-rolodex/static/css/main.ef9e623d.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db60844c964a08055936",
+    "url": "/monsters-rolodex/static/js/main.33561160.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
